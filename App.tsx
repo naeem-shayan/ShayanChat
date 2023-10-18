95c5b2403b96b4dac44471a094dce03fa83fe73e
naeem-shayan <Naeem.Shayansolutions@gmail.com>
2023-10-18 18:45:07 +0500

    Implement the social logins feature in app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import LoginScreen from './src/Screens/LoginScreen';
 import SignupScreen from './src/Screens/SignupScreen';
 import Navigation from './src/Navigator/Navigation';
-import {AuthProvider} from './src/Context/authProvider';
+import { AuthProvider } from './src/Context/authProvider';
 import Toast from 'react-native-toast-message';
-import {PaperProvider} from 'react-native-paper';
+import { PaperProvider } from 'react-native-paper';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+GoogleSignin.configure({
+  webClientId:
+    '9475515452-vlaj16b1r27sqf8joap26ca240cse5va.apps.googleusercontent.com',
+});
+
 
 function App(): JSX.Element {
   return (
