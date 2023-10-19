d258f8688d561263b73bb1285ba247b30a6ca6fb
Asim Khursheed <asimkhurshid313@gmail.com>
2023-10-19 12:18:53 +0500

    miner fixes

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import { AuthProvider } from './src/Context/authProvider';
 import Toast from 'react-native-toast-message';
 import { PaperProvider } from 'react-native-paper';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useEffect, useRef, useState } from 'react';
+import { AppState } from 'react-native';
 GoogleSignin.configure({
   webClientId:
     '9475515452-vlaj16b1r27sqf8joap26ca240cse5va.apps.googleusercontent.com',
