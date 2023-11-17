8b69012eb09b4179f480cf98585ecaea6a335229
Imran <imran@shayansolutions.com>
2023-11-17 11:52:16 +0500

    add redux redux-persist

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,6 +1,24 @@
-import {createStore} from 'redux';
+// import {createStore} from 'redux';
+// import userReducer from '../Reducers/userReducer';
+
+// const store = createStore(userReducer);
+
+// export default store;
+
+import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import userReducer from '../Reducers/userReducer';
 
-const store = createStore(userReducer);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  // Add other configuration options if needed
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, userReducer);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
+
